0a6653710722c9e157ec465077ca1ec75d543e9e
Carol Wanjohi <wanjohi.carol54@gmail.com>
2025-05-03 12:15:18 +0300

    refactor: [GreeterAgent] remove unused import

--- src/main/java/com/example/hello_world/agents/GreaterAgentController.java
+++ src/main/java/com/example/hello_world/agents/GreaterAgentController.java
@@ -1,9 +1,7 @@
 package com.example.hello_world.agents;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
